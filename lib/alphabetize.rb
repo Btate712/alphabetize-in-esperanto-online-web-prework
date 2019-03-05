b85794f81ddb9811c37c13fbbe00ed411f5cdbd9
Robert Tate <btate712@gmail.com>
2019-03-05 09:03:51 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,5 +2,14 @@ require 'pry'
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  arr.sort_by{ |word| ESPERANTO_ALPHABET.index(word[0])}
+  arr.sort_by{ |word| encode_to_ascii(word)}
 end
+
+def encode_to_ascii(word)
+  max = 0
+  for letter in 0..word.length
+    max = letter
+  end
+  binding.pry
+end
+  
