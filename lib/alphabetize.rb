1c193bfc8a6a587b2acf42e4aeaa131e758654d9
Robert Tate <btate712@gmail.com>
2019-03-05 09:17:34 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,10 +5,4 @@ def alphabetize(arr)
   arr.sort_by{ |word| encode_to_ascii(word)}
 end
 
-def encode_to_ascii(word)
-  encoded_word = ""
-  for letter in 0..(word.length - 1)
-    encoded_word += (64 + ESPERANTO_ALPHABET.index(word[letter])).chr
-  end
-  binding.pry
-end
+
