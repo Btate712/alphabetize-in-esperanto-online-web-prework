6b63a924ce77e9827c74da5b929a710e7473f7d7
Robert Tate <btate712@gmail.com>
2019-03-05 09:39:55 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,10 +3,19 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz "
 
 def alphabetize(arr)
   encoded_array = []
+  return_array = []
+  
   arr.each do |word|
     encoded_array = encode(word)
   end
+  
   encoded_array.sort{ |a, b| a <=> b}
+  
+  encoded_array.each do |encoded_word|
+    decoded_array = decode(encoded_word)
+  end
+  
+  decoded_array
 end
 
 def encode(word)
@@ -16,3 +25,12 @@ def encode(word)
   end
   word_array
 end
+
+def decode(word)
+  decoded_array = []
+  for letter in 0..(word.length - 1)
+    decoded_array.push(ESPERANTO_ALPHABET[word[letter]])
+  end
+  decoded_array.join
+end
+    
