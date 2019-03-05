1f4c2f89359041ef66c682fffc19c5965929647f
Robert Tate <btate712@gmail.com>
2019-03-05 08:50:23 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,21 @@
+require 'pry'
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+  longest = 0
+  arr.sort { |a, b| do
+    if a.length > b.length
+      longest = a.length
+    else
+      longest = b.length
+    end
+    for i in 0..longest
+      if ESPERANTO_ALPHABET.index(a[i]) > ESPERANTO_ALPHABET.index(b[i])
+        return 1
+      elsif ESPERANTO_ALPHABET.index(b[i]) < ESPERANTO_ALPHABET.index(a[i])
+        return -1
+      end
+    end
+    return 0
+  end }
 end
