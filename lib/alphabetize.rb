94e15d694b91a80446dcc10488db57183a45403d
Robert Tate <btate712@gmail.com>
2019-03-05 08:56:44 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,20 +2,5 @@ require 'pry'
 ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  longest = 0
-  arr.sort { |a, b| do
-    if a.length > b.length
-      longest = a.length
-    else
-      longest = b.length
-    end
-    for i in 0..longest
-      if ESPERANTO_ALPHABET.index(a[i]) > ESPERANTO_ALPHABET.index(b[i])
-        return 1
-      elsif ESPERANTO_ALPHABET.index(b[i]) < ESPERANTO_ALPHABET.index(a[i])
-        return -1
-      end
-    end
-    return 0
-  end }
+  arr.sort_by{ |word| ESPERANTO_ALPHABET.index(word[0])}
 end
